6532e6834fd4f63c54c3011814a7c92e5ba2aad9
syedmouaazfarrukh <mouaazfarrukh99@gmail.com>
2024-03-23 14:35:48 +0500

    excel-vba-programming

---
+++ macros/read-from-rds.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import requests
+import openpyxl
+
+# Initialize an empty list to store DataFrame chunks
+dfs = []
+
+# Iterate over offsets
+for offset in range(0, 9001, 1000):  # range stops at 10001 to include 10000
+    print("Currently at offset:", offset)
+    # URL of the data with current offset
+    url = f"https://data.edmonton.ca/resource/q7ua-agfg.json?$offset={offset}"
+    
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Extract the data from the response
+        data = response.json()  # Assuming the response is JSON
+        
+        # Read the JSON data into a pandas DataFrame
+        df_chunk = pd.DataFrame(data)
+        
+        # Append DataFrame chunk to the list
+        dfs.append(df_chunk)
+        
+    else:
+        print(f"Failed to retrieve data for offset {offset}. Status code:", response.status_code)
+
+# Concatenate all DataFrame chunks into a single DataFrame
+result_df = pd.concat(dfs, ignore_index=True)
+
+# Now you can work with the combined DataFrame 'result_df'
+# print(result_df)  # Display the first few rows of the DataFrame
+
+# Write the DataFrame to an Excel file
+excel_filename = "output.xlsx"
+result_df.to_excel(excel_filename, index=False)
+
+print(f"DataFrame has been written to {excel_filename}.")
---
+++ macros/vba-basics.md
@@ -0,0 +1,8 @@
+# VBA Programming Basics
+
+## VBA Worksheet Functions
+
+Applications.WorksheetFunctions.sum(range("A1":"A2"))
+Applications.WorksheetFunctions.average(range("A1":"A2"))
+Applications.WorksheetFunctions.mediam(range("A1":"A2"))
+Applications.WorksheetFunctions.sum(range("A1":"A2"))
